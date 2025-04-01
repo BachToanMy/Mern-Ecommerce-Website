286337a0667aa6385e078be8b9c67a4ef3dac538
BachToanMy <btmy1314@gmail.com>
2025-04-01 23:31:26 +0700

    10h30

--- admin/src/components/Loader.jsx
+++ admin/src/components/Loader.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { FaSpinner } from "react-icons/fa6";
 const Loader = () => {
   return (
-    <div className="text-orange-600 text-5xl w-full flex items-center justify-center py-40 ">
-      <FaSpinner className="animate-spin"></FaSpinner>
+    <div className="text-orange-600 text-5xl w-full flex flex-col items-center justify-center py-40 ">
+      <FaSpinner className="animate-spin" />
+      <p className="text-lg text-black font-medium tracking-wide mt-1">Loading...</p>
     </div>
   );
 };
--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { serverUrl } from "../../config";
 import Title from "../components/Title";
 import Loader from "../components/Loader";
+import { Link } from "react-router-dom";
 const List = () => {
   const [list, setList] = useState([]);
   const [isLoading, setLoading] = useState(false);
@@ -29,13 +30,37 @@ const List = () => {
   }, []);
   return (
     <div>
-      {!isLoading ? (
-        <Loader/>
+      {isLoading ? (
+        <Loader />
       ) : (
         <>
-          <div>
+          <div className="flex items-center justify-between">
             <Title>Product List</Title>
+            <Link
+              to={"/add"}
+              className="text-sm font-medium hover:text-blue-600 duration-300 cursor-pointer"
+            >
+              Add Product +
+            </Link>
           </div>
+          {list?.length > 0 ? (
+            <div className="flex flex-col gap-2 mt-2">
+              <div className="grid grid-cols-[1fr_2fr_1fr_1fr_1fr_1fr] items-center py-1 px-2 border border-gray-300 bg-gray-100 text-sm my-1.5">
+                <b>Image</b>
+                <b>Name</b>
+                <b className="hidden md:inline-block">Category</b>
+                <b>Price</b>
+                <b className="text-center">Action</b>
+                <b className="text-center">Edit</b>
+              </div>
+            </div>
+          ) : (
+            <div className="mt-2">
+              <p className="mb-4 text-red-600 font-medium tracking-wide">
+                You have no product in your Database.
+              </p>
+            </div>
+          )}
         </>
       )}
     </div>
