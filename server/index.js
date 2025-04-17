6fc6a52051e89ed09bd4e52b071cdc665f5c4997
BachToanMy <btmy1314@gmail.com>
2025-04-17 15:53:27 +0700

    nodemon->node

--- server/index.js
+++ server/index.js
@@ -1,31 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import dbConnect from './config/mongodb.js';
-import userRouter from './routes/userRouter.js';
-import productRouter from './routes/productRouter.js';
-import connectCloudinary from './config/cloudinary.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import dbConnect from "./config/mongodb.js";
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import connectCloudinary from "./config/cloudinary.js";
 const app = express();
 
 const port = process.env.PORT || 8000;
 app.use(cors());
-app.use(express.json())
-dbConnect()
+app.use(express.json());
+dbConnect();
 connectCloudinary();
-console.log(process.env.PORT)
+console.log(process.env.PORT);
 
-app.get('/',(req,res)=>{
-    res.send("Hello from orebi api server");
+app.get("/", (req, res) => {
+  res.send("Hello from orebi api server");
 });
 
-app.use('/api/user',userRouter);
-app.use('/api/product',productRouter);
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
 
-app.get('/api',(req,res)=>{
-    res.send('this is orebi commerce')
-})
+app.get("/api", (req, res) => {
+  res.send("this is orebi commerce");
+});
 
-
-app.listen(port,()=>{
-    console.log(`server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
 });
